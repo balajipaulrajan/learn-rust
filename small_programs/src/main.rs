341d5b0e8e6d8adfb3d5b98c4ded202f05d468fb
Balaji Paulrajan <balaji.ceg@gmail.com>
2018-05-26 21:40:19 +1000

    F2C

--- small_programs/src/main.rs
+++ small_programs/src/main.rs
@@ -2,10 +2,12 @@ use std::io;
 
 fn main() {
     fibonacci();
+    fahrenheit_to_celsius();
+    celsius_to_fahrenheit();
 }
 
 fn fibonacci() {
-    println!("Enter the number");
+    println!("Enter the number for finding the fibonocci sequence");
     let mut n = String::new();
     io::stdin().read_line(&mut n).expect("Failed to read line");
     let n = n.trim().parse().expect("Expected a number");
@@ -25,4 +27,23 @@ fn fib_internal(n: i64) -> i64 {
     } else {
         fib_internal(n - 1) + fib_internal(n - 2)
     }
+}
+
+fn fahrenheit_to_celsius() {
+    println!(" Enter the temperature in fahrenheit");
+    let n = read_input_number();
+    let c = (n - 32.0) / 1.8;
+    println!("Temperature in celsius is {:.2}", c);
+}
+fn celsius_to_fahrenheit() {
+    println!(" Enter the temperature in celsius");
+    let n = read_input_number();
+    let f = (n * 1.8) + 32.0;
+    println!("Temperature in fahrenheit is {:.2}", f);
+}
+fn read_input_number() -> f64 {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read");
+    let n = n.trim().parse().expect("Expected a number");
+    n
 }
