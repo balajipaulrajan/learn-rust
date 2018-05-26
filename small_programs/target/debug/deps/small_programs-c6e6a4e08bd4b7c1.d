1258185421fda4143f5e1ff3100b7fcab39311e1
Balaji Paulrajan <balaji.ceg@gmail.com>
2018-05-26 20:10:18 +1000

    removed target files

--- small_programs/target/debug/deps/small_programs-c6e6a4e08bd4b7c1.d
+++
@@ -1,5 +0,0 @@
-/Users/balajipaulrajan/rust/learn-rust/small_programs/target/debug/deps/small_programs-c6e6a4e08bd4b7c1: src/main.rs
-
-/Users/balajipaulrajan/rust/learn-rust/small_programs/target/debug/deps/small_programs-c6e6a4e08bd4b7c1.d: src/main.rs
-
-src/main.rs:
--- small_programs/target/debug/small_programs.d
+++
@@ -1,1 +0,0 @@
-/Users/balajipaulrajan/rust/learn-rust/small_programs/target/debug/small_programs.dSYM: /Users/balajipaulrajan/rust/learn-rust/small_programs/src/main.rs
